6c4aee78ccb656b2f0308ddb6b27ea27d47e3b6a
Carlos Henrique <ichfs9108@gmail.com>
2024-01-09 01:51:03 -0300

    Project Created

---
+++ agregadorinvestimentos/agregadorinvestimentos/src/main/java/project/agregadorinvestimentos/AgregadorinvestimentosApplication.java
@@ -0,0 +1,13 @@
+package project.agregadorinvestimentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgregadorinvestimentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgregadorinvestimentosApplication.class, args);
+	}
+
+}
